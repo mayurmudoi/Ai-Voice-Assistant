bb2785524f4d37137b05e1869d09da4c31fa3c6e
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-07-18 12:14:51 +0530

    newProject

---
+++ App.js
@@ -0,0 +1,15 @@
+import { Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+      <Text>hello</Text>
+      <Text>hello</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
