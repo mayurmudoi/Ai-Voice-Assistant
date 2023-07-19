bdd443296a8b5b79dde9f6c019d1e9f73216aaf4
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-07-19 19:42:48 +0530

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const HomeScreen = () => {
   return (
